04fac53f8adf56fd8eb8db9d0c5810d151a3642a
nico_ <nico_@DESKTOP-UI6V6UM>
2017-04-24 21:00:35 -0300

    Primer Commit

---
+++ src/Entities/Lote.java
@@ -0,0 +1,20 @@
+package Entities;
+
+public class Lote {
+	
+	private Long id;
+	private Integer cantDisponible;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public integer getCantDisponible() {
+		return cantDisponible;
+	}
+	public void setCantDisponible(integer cantDisponible) {
+		this.cantDisponible = cantDisponible;
+	}
+	
+}
---
+++ src/Entities/OrdenProduccionCompleta.java
@@ -0,0 +1,15 @@
+package Entities;
+
+public class OrdenProduccionCompleta extends OrdenProduccion{
+
+	private Prenda prenda;
+
+	public Prenda getPrenda() {
+		return prenda;
+	}
+
+	public void setPrenda(Prenda prenda) {
+		this.prenda = prenda;
+	}
+	
+}
